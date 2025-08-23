f534dba06ea7426d767daebc89ea7cea3d382c86
Kimyubin357 <kimyubin357@gmail.com>
2025-08-23 22:58:30 +0900

    Solved 10430 problem

---
+++ 10430.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+let input = fs.readFileSync('example.txt').toString().trim().split(' ').map(Number)
+
+let A = input[0]
+let B = input[1]
+let C = input[2]
+
+console.log((A + B) % C)
+console.log(((A % C) + (B % C)) % C)
+console.log((A * B) % C)
+console.log(((A % C) * (B % C)) % C)
