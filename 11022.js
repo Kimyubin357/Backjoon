ccb76a9d6d923ec13a847b8dcfcf4714200faaca
Kimyubin357 <kimyubin357@gmail.com>
2025-08-26 12:27:56 +0900

    solved 11022 problem

---
+++ 11022.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+let input = fs.readFileSync('example.txt').toString().trim().split('\n')
+
+let test_case_n = Number(input[0])
+let result =''
+for(let i = 1; i <= test_case_n; i++){
+    let user_input = input[i].split(' ').map(Number)
+    let A = user_input[0]
+    let B = user_input[1]
+
+    result += `Case #${i}: ${A} + ${B} = ${A+B}` + '\n'
+}
+console.log(result)
