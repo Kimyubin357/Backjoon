9ca819fb5bd291cfeeb77904c2723cd9967e48c4
Kimyubin357 <kimyubin357@gmail.com>
2025-08-26 12:17:43 +0900

    solved 15552 problem

---
+++ 15552.js
@@ -0,0 +1,13 @@
+//빠른 입출력 문제
+const fs = require('fs');
+let input = fs.readFileSync('example.txt').toString().trim().split('\n')
+
+let test_case_n = Number(input[0])
+let result =''
+for(let i = 1; i <= test_case_n; i++){
+    let user_input = input[i].split(' ').map(Number)
+    let A = user_input[0]
+    let B = user_input[1]
+    result += A+B +'\n'
+}
+console.log(result)
